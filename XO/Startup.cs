c732d38ad4bf79e4780e1bbc5fbbaa020f6ab9c1
Роман <@BESTFORPRICE>
2018-11-30 14:09:45 +0600

    Добавьте файлы проекта.

---
+++ XO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XO.Startup))]
+namespace XO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
